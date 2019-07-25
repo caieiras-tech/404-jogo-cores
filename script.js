480e944c2fce158714d9341c00181d40cd1445a2
Lucas Melo <luucasmell@gmail.com>
2019-07-25 16:00:36 -0300

    adicionar comentarios

--- script.js
+++ script.js
@@ -2,26 +2,34 @@ let quadradinhos = document.querySelectorAll("div");
 let resposta = document.querySelector("h2");
 let botao = document.querySelector("button");
 
+//CRIANDO LISTA DE CORES
 let cores = ["red", "blue", "yellow"];
 
+
+//FUNÇÃO PARA GERAR CORES ALEATORIAS
 function gerarCor(){
     let aleatorio = Math.floor(Math.random()* cores.length);
-    
+    //o THIS está sendo utilizando para se referenciar ao próprio elemento em que está sendo clicado
     this.style.backgroundColor = cores[aleatorio];
+
+    //chamando a função de verificar final, para verificar somente quando gerar uma cor
     verificarFinal();
 }
-
+//Aplicando a função à todos os quadradinhos
 for(let quadrado of quadradinhos){
     quadrado.onclick = gerarCor;
 }
 
+//Função que verifica se todos os quadrados estão com a mesma cor
 function verificarFinal(){
     if(quadradinhos[0].style.backgroundColor == quadradinhos[1].style.backgroundColor && quadradinhos[1].style.backgroundColor == quadradinhos[2].style.backgroundColor && quadradinhos[2].style.backgroundColor == quadradinhos[3].style.backgroundColor){
         resposta.innerHTML = "!!! VOCÊ VENCEU !!!";
-
+        
         for(let quadrado of quadradinhos){
             quadrado.onclick = null;
-            quadrado.classList.add("virarQuadradinho");
+            setTimeout(() => {
+                quadrado.classList.add("virarQuadradinho");
+            }, 1000);
         }  
     }
     else{
@@ -29,9 +37,11 @@ function verificarFinal(){
     }
 }
 
+//Função para resetar o jogo, limpando as cores de fundo e trocando o texto de resposta
 function resetarJogo(){
     for(let quadrado of quadradinhos){
         quadrado.style.backgroundColor = "unset";
+        quadrado.onclick = gerarCor;        
     }
     resposta.innerHTML = "Jogo resetado !";
 }
